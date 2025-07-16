ca50f3362de28429b420cab1e2f7539a2f5e32e1
ankush1423 <139746905+ankush1423@users.noreply.github.com>
2025-07-16 09:17:00 +0530

    secure app using helmet

---
+++ src/controllers/post.controller.js
@@ -0,0 +1,50 @@
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {Post} from "../models/post.model.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+import jwt from "jsonwebtoken"
+
+
+export const createPost = asyncHandler(async(req,res) => {
+     
+})
+
+export const updatePost = asyncHandler(async(req,res) => {
+     
+})
+
+export const deletePost = asyncHandler(async(req,res) => {
+     
+})
+
+export const getPost = asyncHandler(async(req,res) => {
+     
+})
+
+export const getPosts = asyncHandler(async(req,res) => {
+     
+})
+
+export const deleteFile = asyncHandler(async(req,res) => {
+     
+})
+
+export const getFilePreview = asyncHandler(async(req,res) => {
+     
+})
+
+
+
+
+
+
+
+
+
+// createPost
+// updatePost
+// deletePost
+// getPost
+// getPosts
+// deleteFile // from cloudinary
+// getFilePreview // the uploaded image
